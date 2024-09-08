074f6eb0ad0b7be3b47731b1d2ccdb3a9d6b3995
wizjany <wizjany@gmail.com>
2024-09-08 13:40:13 -0400

    Check mob-damage for potion splashes from e.g. witches.
    Note that bypass bypasses this if the player with bypass is the target of the potion, but only for the player with bypass.
    
    Fixes #2134.

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/EventAbstractionListener.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/listener/EventAbstractionListener.java
@@ -1014,7 +1014,6 @@ public class EventAbstractionListener extends AbstractListener {
 
         // Fire item interaction event
         Events.fireToCancel(event, new UseItemEvent(event, cause, world, potion.getItem()));
-
         // Fire entity interaction event
         if (!event.isCancelled()) {
             int blocked = 0;
@@ -1026,8 +1025,11 @@ public class EventAbstractionListener extends AbstractListener {
                         ? new DamageEntityEvent(event, cause, affected) :
                         new UseEntityEvent(event, cause, affected);
 
-                // Consider the potion splash flag
+                // Consider extra relevant flags
                 delegate.getRelevantFlags().add(Flags.POTION_SPLASH);
+                if (potion.getShooter() instanceof LivingEntity shooter && !(shooter instanceof Player) && affected instanceof Player) {
+                    delegate.getRelevantFlags().add(Flags.MOB_DAMAGE);
+                }
 
                 if (Events.fireAndTestCancel(delegate)) {
                     event.setIntensity(affected, 0);
